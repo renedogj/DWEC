e39812f16587a7ef016ac5db5e1026e409f87bbd
renedogj <68063003+renedogj@users.noreply.github.com>
2021-10-04 19:05:33 +0200

    Practica09-11 terminada

---
+++ Practica05/practica05-11/Practica05-11.js
@@ -0,0 +1,47 @@
+function validar(){
+	var localidad = document.formulario.localidad.value;
+	document.formulario.mensaje.value = comprobarLocalidad(localidad);
+}
+
+function comprobarLocalidad(localidad){
+	localidad = localidad.trim();
+	if(localidad.length >= 7 && localidad.length <=35){
+		if(esLetra(localidad[0]) && esLetra(localidad[1]) && esLetra(localidad[2])){
+			if(esLetra(localidad[localidad.length-2]) && esLetra(localidad[localidad.length-1])){
+				if(comprobarCaracteres(localidad.substring(4,localidad.length-2))){
+					return true;
+				}
+				return false;
+			}
+			return false;
+		}
+		return false;
+	}
+	return false;	
+}
+
+function esLetra(letra){
+	letra = letra.toUpperCase();
+	letra = letra.charCodeAt(0);
+	return (letra > 64 && letra < 91) || letra == 209 
+		|| letra == 193 || letra == 201 || letra == 205 
+		|| letra == 211 || letra == 218 || letra == 220;
+}
+
+function comprobarCaracteres(cadena){
+	var valido = true;
+	for(var i = 0;i < cadena.length; i++){
+		if(esLetra(cadena[i])){
+			valido = true;
+		}else{
+			valido = (cadena[i] == " ");
+		}	
+	}
+	return valido;
+}
+
+function esSigno(signo){
+	return signo == "." || signo == "_" || signo == "-";
+}
+
+document.formulario.boton.onclick = validar;
